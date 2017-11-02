8af9d3ee5cf5d638ce2bf8d821578f52396de7f6
Rizwan Khan <rizwan.office1979@gmail.com>
2017-11-02 14:48:37 +0400

    carousal to contact page

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from damatex_overseas_app.models import CarousalImages
 from .forms import ContactForm
 # Create your views here.
 
 
 def contact(request):
+    carosual_images = CarousalImages.objects.all()
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'contact/contacted.html',{})
+            return render(request, 'contact/contacted.html',{'carosual_images': carosual_images})
     else:
         form = ContactForm()
 
-    return render(request, 'contact/contact.html', {'form': form})
+    return render(request, 'contact/contact.html', {'form': form, 'carosual_images': carosual_images})
